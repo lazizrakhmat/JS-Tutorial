5f24db38ba1f9761255ae93a7f94b64f70f97e08
Lazizbek Rakhmatov <lazizrakhmat@gmail.com>
2023-06-08 15:18:10 +0500

    added a new  "if else" lesson

---
+++ script.js
@@ -0,0 +1,26 @@
+let age = +prompt ('How old are you?')
+
+// IF ELSE
+if(age < 18) {
+    alert('your age is not enough to use this site :(')
+} else if (age > 65) {
+    alert('you are already late')
+}else{
+    alert('Welcome :)')
+}
+
+
+
+// SWITCH CASE
+
+/*
+switch (age) {
+    case 18:
+        alert("you are 18 years old")
+        break
+    case 45:
+        alert("you are 45 years old")
+        break
+    default:
+        alert("your age unknown")
+}*/
